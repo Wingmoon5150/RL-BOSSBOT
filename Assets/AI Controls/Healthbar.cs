949bbe01ec1ecd5b43f2f0a9cb57b404da929c81
Wingmoon5150 <54482@student.eadania.dk>
2025-02-20 12:45:13 +0100

    added lose and win scenes

--- Assets/AI Controls/Healthbar.cs
+++ Assets/AI Controls/Healthbar.cs
@@ -1,11 +1,16 @@
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Healthbar : MonoBehaviour
 {
     [SerializeField] private Helth health;
+    [SerializeField] private string scene;
     // Update is called once per frame
     void Update()
     {
         transform.localScale = new Vector3(health.HP/30.5f, 0.6f, 1f);
+        if (health.HP <= 0)
+            SceneManager.LoadScene(scene);
     }
 }
