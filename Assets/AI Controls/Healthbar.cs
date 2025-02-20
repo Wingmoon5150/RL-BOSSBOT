97d7101ff3912171a95bbad5bbfa0ca37674b129
Wingmoon5150 <54482@student.eadania.dk>
2025-02-20 12:06:48 +0100

    fixed issues

--- Assets/AI Controls/Healthbar.cs
+++ Assets/AI Controls/Healthbar.cs
@@ -6,6 +6,6 @@ public class Healthbar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.localScale = new Vector3(health.HP/10.5f, 0.6f, 1f);
+        transform.localScale = new Vector3(health.HP/30.5f, 0.6f, 1f);
     }
 }
